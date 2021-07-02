1a8a965bea818f72888198153d1a9ba6e2c33a5e
Björn Löfroth <bjorn.lofroth@irori.se>
2021-07-02 09:40:15 +0200

    feat: initial Maven structure
    using archetype `mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4`
    
    irori-ab/claim-check-interceptors-planning#1

---
+++ src/main/java/se/irori/kafka/claimcheck/App.java
@@ -0,0 +1,13 @@
+package se.irori.kafka.claimcheck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
